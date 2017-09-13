da4d0b8245df108cdc8856891898618cb18448d6
Bowen Sun <bowensun@gmail.com>
2017-09-13 20:15:22 +0100

    fix aws config

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
+
 export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
 FILENAME=$(date +%s) 
 
 echo "Backup Start @" $(date -d @$FILENAME)
 
-/dynamodump.py -m backup -a tar -s "$DYNAMODB_TABLE" -b "$S3_BUCKET" -r "$AWS_DEFAULT_REGION" --dumpPath "$FILENAME" --log INFO >> /var/log/cron.log 2>&1 && rm -rf $FILENAME $FILENAME.tar.bz2
+/dynamodump.py -m backup -a tar -s $DYNAMODB_TABLE -b $S3_BUCKET -r $AWS_DEFAULT_REGION --dumpPath $FILENAME --log INFO >> /var/log/cron.log 2>&1 && rm -rf $FILENAME $FILENAME.tar.bz2
 
